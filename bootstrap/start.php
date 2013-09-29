05201793dcac6db23300979bd31593293b73e82a
Gil Rutkowski <gil@domandtom.com>
2013-09-29 14:00:39 -0500

    Basic Config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,9 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local'			=> array( 'localhost', '127.0.0.1', '*.dev' ),
+	'staging' 		=> array( 'beta.flashingcursor.com' ),
+	'production' 	=> array( 'www.flashingcursor.com', 'flashingcursor.com' )
 
 ));
 
